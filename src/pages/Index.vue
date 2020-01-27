653f611f711a3411eabb8a3ddb4a438e2d8962ad
Lenasurlenet <lenasurlenet@gmail.com>
2020-01-27 13:14:55 +0100

    First commit. Set up with Remark and Netlify CMS

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout>
+
+    <h1>Do things right</h1>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
